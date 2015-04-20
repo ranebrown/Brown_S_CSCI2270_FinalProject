ec5e79c1371c0e63856dcdb18778b04111e23da4
ranebrown <rane.brown@gmail.com>
2015-04-20 16:24:22 -0600

    Created two structs and a class for use with program

--- cppWebSearch.h
+++ cppWebSearch.h
@@ -7,6 +7,26 @@
 #ifndef cppWebSearch_H 
 #define cppWebSearch_H
 
+#include <iostream>
+#include <boost/network/protocol/http/client.hpp>
 
+struct WebQueue {
+	
+}
+
+struct WordHash {
+
+}
+
+class WebSearch {
+	public:
+		WebSearch();
+		virtual ~WebSearch();
+		void EnqueueSite(std::string address);
+	protected:
+
+	private:
+
+}
 
 #endif // cppWebSearch_H 
