fc8aaf2938f5bb17937e87909ea5e0d5c40f6a66
ranebrown <rane.brown@gmail.com>
2015-04-23 13:51:15 -0600

    Modified BuildQueue function to catch exception if invalid url entered. Used a temp queue for scanning urls to avoid repeats

--- cppWebSearch.cpp
+++ cppWebSearch.cpp
@@ -7,7 +7,6 @@
 #include "cppWebSearch.h"
 #include <boost/network/protocol/http/client.hpp>
 #include <string>
-#include <queue>
 #include <fstream>
 #include <unistd.h> // for usleep function
 
@@ -41,46 +40,94 @@ void WebSearch::EnqueueSite(std::string url) {
 */
 void WebSearch::BuildQueue(std::string url, int depth) {
 	using namespace boost::network;
-	std::queue<std::string> urlList; // queue to store list of urls up to specified depth
 	urlList.push(url); // add url to queue
+	bool scan = true;
+	std::queue<std::string> tempQ;
+	tempQ.push(url);
+
 	while((int)urlList.size() < depth) {
-		url = urlList.front(); // dequeue a url and scan for more links
-		usleep(5000); // 5000 microsecond delay
+		scan = true;
+		url = tempQ.front(); // extract next url and scan for more links
+		tempQ.pop();
+		usleep(200000); // .2 second delay
 
 		// create connection to url and save html code to a text file
-   		http::client client;
-    	http::client::request request(url);
-    	request << header("Connection", "close");
-    	http::client::response response = client.get(request);
-		std::ofstream tempOut("temp.txt");
-		if(tempOut.is_open()) {
-			tempOut << body(response);
-			tempOut << "\n";
-		}
-		tempOut.close();
-	
-		// open the text file and scan for urls
-		std::ifstream tempIn("temp.txt");
-		if(!tempIn.is_open())
-			std::cout<<"Error opening file\n";
-		else {
-			std::string temp;
-			std::size_t start, end;
-			while(getline(tempIn,temp,' ')) {
-				start = temp.find("http://www."); // find the beginning of a new url
-				end = temp.find('"',start); // find the end of the url (assuming url in inside "quotes")
-				if(start != std::string::npos && end != std::string::npos) {
-					std::string foundURL = temp.substr(start, (end-start)); // extract only the url
-					urlList.push(foundURL);
-				}	
+   		try {
+			std::cout<<"Processing: "<<url<<std::endl;
+			http::client client;
+    		http::client::request request(url);
+    		request << header("Connection", "close");
+    		http::client::response response = client.get(request);
+			remove("temp.txt"); // delete old temporary text file
+			std::ofstream tempOut("temp.txt");
+			if(tempOut.is_open()) {
+				tempOut << body(response);
+				tempOut << "\n";
 			}
-		}	
+			tempOut.close();
+		}
+		catch(boost::system::system_error const& ec) {
+			std::cout<<"Connection error to url: "<<url<<std::endl;
+			scan = false;
+			if(urlList.size() ==1)
+				break;
+		}
+		if(scan == true) {
+			// open the text file and scan for urls
+			std::ifstream tempIn("temp.txt");
+			if(!tempIn.is_open())
+				std::cout<<"Error opening file\n";
+			else {
+				std::string temp;
+				std::size_t start, end;
+				bool unique;
+				while(getline(tempIn,temp,' ')) {
+					start = temp.find("http://www."); // find the beginning of a new url
+					end = temp.find('"',start); // find the end of the url (assuming url in inside "quotes")
+					if(start != std::string::npos && end != std::string::npos) {
+						std::string foundURL = temp.substr(start, (end-start)); // extract only the url
+						unique = isUnique(foundURL); // check if found url is unique (hasn't already been found)
+						if(unique == true) {
+							urlList.push(foundURL);
+							tempQ.push(foundURL);
+						}
+					}	
+				}
+			}	
+		}
 	}
 	while(!urlList.empty()) {
 		url = urlList.front();
 		urlList.pop();
 		std::cout<<url<<std::endl;
 	}
+}
+
+/*
+* Takes a url as input and checks if that url has already been added to queue.
+* If it has already been added returns false. Otherwise returns true.
+*/
+bool WebSearch::isUnique(std::string url) {
+	bool unique = true;
+	std::queue<std::string> tempQueue;
+	std::string dqURL;
+
+	// copy queue to temp location
+	while(!urlList.empty()) {
+		dqURL = urlList.front();
+		urlList.pop();
+		tempQueue.push(dqURL);
+	}
+	
+	// check if url has already been added
+	while(!tempQueue.empty()) {
+		dqURL = tempQueue.front();
+		tempQueue.pop();
+		if(dqURL == url)
+			unique = false;
+		urlList.push(dqURL); // copy back to original queue
+	}		
+	return unique;
 }
 
 /*
